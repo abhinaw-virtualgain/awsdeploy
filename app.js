53133f15718d5de3e3b274012d8bbb91c6748491
abhinaw-tech <abhinawgumla@gmail.com>
2020-08-25 17:44:33 +0530

    change port

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send('We are at home');
-})
+});
 
-app.listen(3000);
+const port = process.env.port || 3000;
+
+app.listen(port,()=>{
+    console.log("Abhi");
+});
 
